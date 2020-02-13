324cc29b4cb411e488d719646b23c9d4e3514ddb
datasinfo <autodataxx@gmail.com>
2020-02-13 16:04:02 -0500

    aadd

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Header from '../components/header';
+
+export default function Index() {
+    return (
+        <div>
+            <Header/>
+            <p>hello</p>
+        </div>
+    );
+}
